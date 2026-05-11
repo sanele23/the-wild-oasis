0f29a9d9ce97e1dd8be1bb0b6857fbc469901937
Sanele Krakra <sanele23@live.com>
2026-05-11 22:36:57 +0200

    feat: expanding on styled components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,30 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: blue;
-  color: white;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 8px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 0.8rem 1.2rem;
-`;
-
-const StyledApp = styled.div`
+const StyledApp = styled.main`
   background-color: green;
   padding: 20px;
 `;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>Hello World</H1>
-      <Button onClick={() => alert("Check in clicked!")}>Check in</Button>
-      <Button onClick={() => alert("Check out clicked!")}>Check out</Button>
-      <Input placeholder="Number of guests..." />
-      <Input placeholder="Number of guests..." />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => alert("Check in clicked!")}>Check in</Button>
+        <Button onClick={() => alert("Check out clicked!")}>Check out</Button>
+
+        <Heading as="h3">Form</Heading>
+        <Input placeholder="Number of guests..." />
+        <Input placeholder="Number of guests..." />
+      </StyledApp>
+    </>
   );
 }
 
